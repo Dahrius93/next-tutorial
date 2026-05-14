0cb192aec737b13befec7e132ac1a4f02ca3d742
Dahrius93 <dariolauretta93@gmail.com>
2026-05-14 19:58:06 +0200

    refactor fetchTours

--- app/counter/page.tsx
+++ app/counter/page.tsx
@@ -3,7 +3,7 @@ import Counter from "@/components/Counter";
 function CounterPage() {
   return (
     <>
-      <h1>Page counter content</h1>
+      <h1 className="text-6xl mb-16">Page Content</h1>
       <Counter />
     </>
   );
