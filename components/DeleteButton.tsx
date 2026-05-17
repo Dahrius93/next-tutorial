c453e8d54f8c09af6f9aa155c21514a39a77d70a
Dahrius93 <dariolauretta93@gmail.com>
2026-05-17 08:29:02 +0200

    removeUser with id bind

--- components/DeleteButton.tsx
+++ components/DeleteButton.tsx
@@ -4,7 +4,7 @@ function DeleteButton({ id }: { id: string }) {
   const removeUserWithId = removeUser.bind(null, id);
   return (
     <form action={removeUserWithId}>
-      <input type="hidden" name="name" value={id} />
+      <input type="hidden" name="name" value="shakeAndBake" />
       <button
         type="submit"
         className="bg-red-500 text-white text-xs rounded p-2"
